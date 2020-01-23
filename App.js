5a9d284d63298a484696c72b8ff08ec46d6697c0
Andres Mejias <aemabit@gmail.com>
2020-01-22 19:54:43 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { SafeAreaView } from 'react-native'
+import { TasksContextProvider } from './context/TasksContext'
+import TasksList from './components/TasksList'
+import Form from './components/Form'
+ 
+export default () => (
+  <TasksContextProvider>
+    <SafeAreaView>
+      <Form />
+      <TasksList />
+    </SafeAreaView>
+  </TasksContextProvider>
+)
